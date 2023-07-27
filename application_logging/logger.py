ce81971b11764f4d269698fecd4f14c616fa7d18
Prakhar2295 <prakhar.kumar5@gmail.com>
2023-07-27 14:50:44 +0530

    logging.py mistake correction done

--- application_logging/logger.py
+++ application_logging/logger.py
@@ -19,12 +19,11 @@ class App_Logger:
     def __init__(self):
         pass
 
-    def log(self,file_object,log_message):
+    def log(self,file_object,log_message:str):
         self.now = datetime.now()
         self.date = self.now.date()
         self.current_time = self.now.strftime("%H:%M:%S")
-        file_object.write
-        (str(self.date) + "/" + str(self.current_time) + "\t\t" + log_message + "\n")
+        file_object.write(str(self.date) + "/" + str(self.current_time) + "\t\t" + log_message + "\n")
 
 
 

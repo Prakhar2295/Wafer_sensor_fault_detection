dd61642ad2e14e79fd32aefbe851377c5b4ea286
Prakhar2295 <prakhar.kumar5@gmail.com>
2023-07-27 15:54:58 +0530

    values from schema method updated

--- Training_raw_data_validation/rawvalidation.py
+++ Training_raw_data_validation/rawvalidation.py
@@ -27,7 +27,56 @@ class Raw_Data_Validation:
         self.schema_path = "schema_training.json"
         self.logger = App_Logger()
 
+    def valuesFromSchema(self):
+
+        try:
+            with open(self.schema_path,'r') as f:
+                dic = json.load(f)
+                f.close()
+                pattern = dic["SampleFileName"]
+                LengthOfDateStampInFile = dic["LengthOfDateStampInFile"]
+                LengthOfTimeStampInFile = dic["LengthOfTimeStampInFile"]
+                column_names = dic["ColName"]
+                NumberofColumns = dic["NumberofColumns"]
+                file = open("Training_Logs/valuesfromSchemavalidationLogs.txt",'a+')
+                log_message = "LengthOfDateStampInFile:: %s" %LengthOfDateStampInFile + "\t" + "LengthOfTimeStampInFile:: %s" %LengthOfTimeStampInFile + "\t" + "NumberofColumns:: %s" %NumberofColumns + "\t" + "Filename:: %s" %pattern + "\n"
+                self.logger.log(file,log_message)
+
+                file.close()
+
+        except ValueError:
+            file = open("Training_Logs/valuesfromSchemavalidationLogs.txt",'a+')
+            self.logger.log(file,"ValueError: Value not found inside schema_taining.json")
+            file.close()
+            raise ValueError
         
+        except KeyError:
+            file = open("Training_Logs/valuesfromSchemavalidationLogs.txt",'a+')
+            self.logger.log(file,"KeyError: Key value error incorret key passes")
+            file.close()
+
+        except Exception as e:
+            file = open("Training_Logs/valuesfromSchemavalidationLogs.txt",'a+')
+            self.logger.log(file,str(e))
+            file.close()
+            raise e
+        
+        return LengthOfDateStampInFile,LengthOfTimeStampInFile,column_names,NumberofColumns
+    
+    def manualregexcreation(self):
+        pass
+                
+
+
+
+
+
+
+
+
+
+
+
 
 
 

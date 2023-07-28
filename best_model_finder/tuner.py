514df90af10697e35357785e2b1182ce826fefbb
Prakhar2295 <prakhar.kumar5@gmail.com>
2023-07-28 23:52:31 +0530

    get_best_param_for_random_forest method created

---
+++ best_model_finder/tuner.py
@@ -0,0 +1,6 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from xgboost import XGBClassifier
+from sklearn.metrics import roc_auc_score, accuracy_score
+
+

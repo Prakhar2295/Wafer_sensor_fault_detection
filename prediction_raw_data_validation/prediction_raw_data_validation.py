ad880b5baa0b4943bf36716650c8091aee85cc4f
Prakhar2295 <prakhar.kumar5@gmail.com>
2023-07-30 08:25:11 +0530

    values_from_schema method and regex method created inside prediction raw data validation

---
+++ prediction_raw_data_validation/prediction_raw_data_validation.py
@@ -0,0 +1,145 @@
+import os
+from datetime import datetime
+import re
+import json
+import shutil
+import pandas as pd
+from application_logging.logger import App_Logger
+
+
+
+class prediction_data_validation:
+    """
+               This class shall be used for handling all the validation done on the Raw Prediction Data!!.
+
+               Written By: JSL
+               Version: 1.0
+               Revisions: None
+
+               """
+
+    def __init__(self,path):
+        self.Batch_Directory = path
+        self.schema_path = 'schema_prediction.json'
+        #self.logger = App_Logger()
+
+
+    def values_from_schema(self):
+        """
+                                Method Name: values_from_schema
+                                Description: This method extracts all the relevant information from the pre-defined "Schema" file.
+                                Output: LengthOfDateStampInFile, LengthOfTimeStampInFile, column_names, Number of Columns
+                                On Failure: Raise ValueError,KeyError,Exception
+
+                                Written By: JSL
+                                Version: 1.0
+                                Revisions: None
+
+                                        """
+        try:
+            with open(self.schema_path, 'r') as f:
+                dic = json.load(f)
+                f.close()
+            pattern = dic['SampleFileName']
+            LengthOfDateStampInFile = dic['LengthOfDateStampInFile']
+            LengthOfTimeStampInFile = dic['LengthOfTimeStampInFile']
+            column_names = dic['ColName']
+            NumberofColumns = dic['NumberofColumns']
+
+            file = open("prediction_logs/valuesfromSchemaValidationLog.txt", 'a+')
+            message ="LengthOfDateStampInFile:: %s" %LengthOfDateStampInFile + "\t" + "LengthOfTimeStampInFile:: %s" % LengthOfTimeStampInFile +"\t " + "NumberofColumns:: %s" % NumberofColumns + "\n"
+            self.logger.log(file,message)
+
+            file.close()
+
+
+
+        except ValueError:
+            file = open("prediction_Logs/valuesfromSchemaValidationLog.txt", 'a+')
+            self.logger.log(file,"ValueError:Value not found inside schema_training.json")
+            file.close()
+            raise ValueError
+
+        except KeyError:
+            file = open("prediction_Logs/valuesfromSchemaValidationLog.txt", 'a+')
+            self.logger.log(file, "KeyError:Key value error incorrect key passed")
+            file.close()
+            raise KeyError
+
+        except Exception as e:
+            file = open("prediction_Logs/valuesfromSchemaValidationLog.txt", 'a+')
+            self.logger.log(file, str(e))
+            file.close()
+            raise e
+
+        return LengthOfDateStampInFile, LengthOfTimeStampInFile, column_names, NumberofColumns
+    
+    def regex_creation(self):
+        """
+              Method: regex_creation
+              Description: This method will create a new regex which will be used 
+              to validate the prediction file names.
+
+              Output: Regex
+              On Failure: None
+
+              Written By: JSL
+              Version: 1.0
+              Revisions: None
+ 
+        
+        """
+        regex = "[\wafer]+['\_'']+[\d_]+[\d]+\.csv"
+        return regex
+    
+
+
+
+
+
+            
+        
+            
+            
+           
+		
+        
+	
+	   
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+   
+	
+	
+	
+
+		
+
+	
+	   
+
+	
+
+
+
+
+
+
+
+
+
+
+
+

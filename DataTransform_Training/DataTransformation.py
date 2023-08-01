2a86433d0e4a8524f1dfdfb960d76d6205f73451
Prakhar2295 <prakhar.kumar5@gmail.com>
2023-08-01 22:19:43 +0530

    data transformation issue resolved

--- DataTransform_Training/DataTransformation.py
+++ DataTransform_Training/DataTransformation.py
@@ -47,8 +47,7 @@ class dataTransform:
             for file in onlyfiles:
                 csv = pandas.read_csv(self.goodDataPath + "/" + file)
                 csv.fillna("NULL",inplace = True)
-
-                csv["wafer"] = csv["wafer"].str[6:]
+                csv["Wafer"] = csv["Wafer"].str[6:]
                 csv.to_csv(self.goodDataPath + "/" + file,index = None,header= True)
                 self.logger.log(log_file,"%s: File Transformed succesfully!!" %file)
                 
